dda825d97ce6f45730e4c396a3c6d80ecddb1848
Hideki Tsuchiya <htsuchiya@htsuchiya.local>
2019-05-06 10:53:43 +0900

    lambda側作成

---
+++ src/event.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+
+exports.api = () => {
+  const url = 'https://connpass.com/api/v1/event/'
+  const params = {
+    series_id: process.env.CONNPASS_SERIES_ID
+  }
+  return axios.get(url, {params}).then((result) => {
+    return result.data.events
+  })
+}
+
+exports.handler = (event, context, callback) => {
+  exports.api().then((result) => {
+    console.log(result)
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(result)
+    })
+  })
+}
